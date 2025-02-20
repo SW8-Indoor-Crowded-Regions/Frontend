11a8f07a7c826b6fe68c4852fcf37ebe2d9dcd9b
Mafusn <Magnus.peetz.holt@gmail.com>
2025-02-20 11:51:18 +0100

    Error fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:indoor_crowded_regions/main.dart';
+import 'package:indoor_crowded_regions/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
