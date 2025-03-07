241fe71d4efa381c41e04b5e180201f854f59ee0
MaizeHH <92512497+MaizeHH@users.noreply.github.com>
2025-03-07 10:43:24 +0100

    feat: user marker indicates heading
    - new icon has been used and is rotated based on user's heading
    - NOTE: works on emulator, but hasn't been tested on physical device yet

--- lib/ui/widgets/user_location_widget.dart
+++ lib/ui/widgets/user_location_widget.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:location/location.dart';
 import 'package:latlong2/latlong.dart';
+import 'dart:math' as math;
 
 class UserLocationWidget extends StatefulWidget {
   final MapController mapController;
@@ -24,6 +24,7 @@ class _UserLocationWidgetState extends State<UserLocationWidget> {
   PermissionStatus _permissionGranted = PermissionStatus.denied;
   LocationData? _locationData;
   bool hasAlteredMap = false;
+  double heading = 0.0;
 
   @override
   void initState() {
@@ -65,6 +66,11 @@ class _UserLocationWidgetState extends State<UserLocationWidget> {
 
   void updateLocation() {
     location.onLocationChanged.listen((LocationData newLocation) {
+      if (newLocation.heading != null) {
+        setState(() {
+          heading = newLocation.heading!;
+        });
+      }
       if (_locationData == null ||
         calculateDistance(_locationData!.latitude!, _locationData!.longitude!,
                           newLocation.latitude!, newLocation.longitude!) > 1) {
@@ -92,11 +98,25 @@ class _UserLocationWidgetState extends State<UserLocationWidget> {
           point: LatLng(_locationData!.latitude!, _locationData!.longitude!),
           width: 30,
           height: 30,
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue,
-              border: Border.all(color: Colors.white, width: 2),
+          child: Transform.rotate(
+            angle: heading * (math.pi / 180),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 35,
+                  height: 35,
+                  decoration: const BoxDecoration(
+                    color: Colors.white, 
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                const Icon(
+                  Icons.assistant_navigation,
+                  color: Colors.blue,
+                  size: 30, 
+                ),
+              ],
             ),
           ),
         ),
