c5f67b059fc86c2588505ccfc9203fdc68924095
Mafusn <Magnus.peetz.holt@gmail.com>
2025-03-27 11:21:03 +0100

    Fix: Returns sensorIds instead of Response class

--- lib/services/gateway_service.dart
+++ lib/services/gateway_service.dart
@@ -4,20 +4,21 @@ class GatewayService {
   final dio = Dio();
   final String baseUrl = "http://localhost:8000/";
 
-  Future<Response> getFastestRoute(String source, String target) async {
+  Future<List<String>> getFastestRouteIds(String source, String target) async {
     try {
-      Response response;
-      response = await dio.post(
+      Response response = await dio.post(
         "${baseUrl}fastest-path",
         data: {
           "source": source,
           "target": target,
         },
       );
-      print(response.data);
-      return response;
+
+      List<String> ids = List<String>.from(response.data['fastest_path']);
+      return ids;
+
     } catch (e) {
-      throw Exception("Failed to fetch fastest route: $e");
+      throw Exception("Failed to fetch fastest route IDs: $e");
     }
   }
 }
