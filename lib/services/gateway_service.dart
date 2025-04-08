fcc07d88fb43a0dfdc2f5fe8e0039d721595f749
Mafusn <Magnus.peetz.holt@gmail.com>
2025-04-08 10:34:55 +0200

    Feat: Using dotenv instead of hardcoded URL

--- lib/services/gateway_service.dart
+++ lib/services/gateway_service.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class GatewayService {
   final dio = Dio();
-  final String baseUrl = "http://127.0.0.1:8000";
 
   Future<List<Map<String, dynamic>>> getFastestRouteWithCoordinates(String source, String target) async {
     try {
+      final String baseUrl = dotenv.env['BASE_URL'] ?? "http://localhost:8000";
       Response response = await dio.post(
         "$baseUrl/fastest-path",
         data: {
