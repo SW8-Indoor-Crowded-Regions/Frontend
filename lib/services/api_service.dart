30cd39247840abb9a8abf3935dde81fb6721213d
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2025-04-22 13:53:09 +0200

    feat: error toasts for services

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,30 +1,35 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:indoor_crowded_regions_frontend/ui/components/error_toast.dart';
 
 class APIService {
-  final dio = Dio();
-  final String baseUrl = "http://localhost:8000/";
+  final dio = Dio(BaseOptions(
+    baseUrl: dotenv.env['baseUrl'] ?? "http://localhost:8000",
+  ));
 
   Future<Response> searchArtwork(String query) async {
     try {
       Response response;
-      response = await dio.get("${baseUrl}search-artwork", queryParameters: {"keys": query});
+      response = await dio.get("search-artwork", queryParameters: {"keys": query});
       return response;
     } catch (e) {
-      throw Exception("Failed to fetch artwork");
+      ErrorToast.show("Failed to fetch artwork");
+      rethrow;
     }
   }
 
   Future<Response> getArtwork(String query, {int rows = 20, int offset = 0}) async {
     try {
       Response response;
-      response = await dio.get("${baseUrl}artwork", queryParameters: {
+      response = await dio.get("artwork", queryParameters: {
         "keys": query,
         "rows": rows,
         "offset": offset,
       });
       return response;
     } catch (e) {
-      throw Exception("Failed to fetch artwork");
+      ErrorToast.show("Failed to fetch artwork");
+      rethrow;
     }
   }
 
